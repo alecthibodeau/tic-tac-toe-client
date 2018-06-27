82dce39a23cdc7bfe163af36a201056602e9adea
alecthibodeau <alecthibodeau@gmail.com>
2018-06-27 14:50:01 -0400

    Add code to API page in auth directory.
    Mix of console log confirmations and error messages.

--- curl-scripts/sign-out.sh
+++
@@ -1,8 +0,0 @@
-# ID=2 sh curl-scripts/sign-out.sh
-
-curl "https://tic-tac-toe-wdi.herokuapp.com/sign-out" \
-  --include \
-  --request DELETE \
-  --header "Authorization: Token token=${TOKEN}" \
-
-echo
