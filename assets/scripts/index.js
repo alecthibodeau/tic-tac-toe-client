fdda1d06072be7c479674c8ae74fea174b202ab3
alecthibodeau <alecthibodeau@gmail.com>
2018-07-08 22:30:01 -0400

    Saving newly merged files.
    The ones with blue dots in Atom.

---
+++ assets/scripts/index.js
@@ -0,0 +1,37 @@
+'use strict'
+
+// use require with a reference to bundle the file and use it in this file
+// const example = require('./example')
+//
+// use require without a reference to ensure a file is bundled
+// require('./example')
+//
+// const squareEvents = require('./gameplay')
+//
+// $(() => {
+//   // your JS code goes here
+//   $('#square-0-0').on('click', squareEvents.onClickSquare)
+// })
+//
+// const authEvents = require('../../curl-scripts/events')
+//
+
+const authEvents = require('./auth/events')
+const gameEvents = require('./game/events')
+
+const preGame = true
+
+// On document ready
+$(() => {
+  authEvents.addAuthHandlers()
+  gameEvents.addGameHandlers()
+  gameEvents.createGameBoard(preGame)
+  // Code to reset modal fields to default upon close…
+  $('body').on('hidden.bs.modal', '.modal', function () {
+    $(this).find('input[type="text"],input[type="email"],input[type="password"],textarea,select').each(function () {
+      if (this.defaultValue !== '' || this.value !== this.defaultValue) {
+        this.value = this.defaultValue
+      } else { this.value = '' }
+    })
+  })
+})
