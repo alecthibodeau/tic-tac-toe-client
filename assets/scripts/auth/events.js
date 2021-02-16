a3092218290f15300d065bba589fc78e4b90f2ef
alecthibodeau <alecthibodeau@gmail.com>
2021-02-16 07:00:06 -0500

    Add password validation. More: - Use RegEx to validate password for Chrome. - Add new modal area for user feedback message.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -12,8 +12,27 @@ const ellipticalWait = function () {
   textTitle.text(textMessage)
 }
 
+const validatePassword = (form) => {
+  const elements = form.elements || []
+  for (let i = 0; i < elements.length; i++) {
+    const e = elements[i]
+    if (e.hasAttribute('name')) {
+      if ((e.getAttribute('name') === 'credentials[password]' || e.getAttribute('name') === 'passwords[new]')) {
+        // Password must be a minimum of 12 characters long, containing at least one letter and one number.
+        const passwordValidation = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{12,}$/);
+        const validPassword = passwordValidation.test(e.value);
+        void (!validPassword && $('.password-validation-message').addClass('validation-error'));
+        return validPassword;
+      }
+    }
+  }
+}
+
 const onSignUp = function (event) {
   event.preventDefault()
+  if (!validatePassword(this)) {
+    return;
+  }
   // $('#modalTitleSignUp').text('Signing up…')
   textTitle = $('#modalTitleSignUp')
   textMessage = 'Signing up…'
@@ -26,6 +45,9 @@ const onSignUp = function (event) {
 
 const onSignIn = function (event) {
   event.preventDefault()
+  if (!validatePassword(this)) {
+    return;
+  }
   // $('#modalTitleSignIn').text('Signing in…')
   textTitle = $('#modalTitleSignIn')
   textMessage = 'Signing in…'
@@ -38,6 +60,9 @@ const onSignIn = function (event) {
 
 const onChangePassword = function (event) {
   event.preventDefault()
+  if (!validatePassword(this)) {
+    return;
+  }
   const data = getFormFields(this)
   api.changePassword(data)
     .then(ui.changePasswordSuccess)
@@ -51,11 +76,16 @@ const onSignOut = function (event) {
     .catch(ui.signOutFailure)
 }
 
+const removeValidationMessage = () => {
+  $('.password-validation-message').removeClass('validation-error');
+}
+
 const addAuthHandlers = () => {
   $('#sign-up').on('submit', onSignUp)
   $('#sign-in').on('submit', onSignIn)
   $('#nav-sign-out').on('click', onSignOut)
   $('#change-password').on('submit', onChangePassword)
+  $('.validation-related-button').on('click', removeValidationMessage);
 }
 
 module.exports = {
